8cb184993c360c9bdd53b0c237e20c7646a9f914
Jane Volada <janevolada@gmail.com>
2022-09-11 18:01:21 +0300

    added the ability to select the maximum number of radio stations

--- src/test/java/ru/netology/radioListener/RadioTest.java
+++ src/test/java/ru/netology/radioListener/RadioTest.java
@@ -8,12 +8,24 @@ import org.junit.jupiter.params.provider.CsvFileSource;
 public class RadioTest {
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/shouldNextRadioStation.csv")
+    @CsvFileSource(files = "src/test/resources/shouldNextRadioStationDefault.csv")
 
-    public void shouldNextRadioStation(int expected, int newCurrentRadioStation) {
+    public void shouldNextRadioStationDefault(int expected, int newCurrentRadioStation) {
         Radio station = new Radio();
         station.currentRadioStation = newCurrentRadioStation;
-        station.Next();
+        station.next();
+        int actual = station.currentRadioStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/shouldNextRadioStation.csv")
+
+    public void shouldNextRadioStation(int expected, int newCurrentRadioStation, int maxRadioStation) {
+        Radio station = new Radio(maxRadioStation);
+        station.currentRadioStation = newCurrentRadioStation;
+        station.next();
         int actual = station.currentRadioStation;
 
         Assertions.assertEquals(expected, actual);
@@ -21,8 +33,8 @@ public class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/shouldPrevRadioStation.csv")
-    public void shouldPervRadioStation(int expected, int newCurrentRadioStation) {
+    @CsvFileSource(files = "src/test/resources/shouldPrevRadioStationDefault.csv")
+    public void shouldPervRadioStationDefault(int expected, int newCurrentRadioStation) {
         Radio station = new Radio();
         station.currentRadioStation = newCurrentRadioStation;
         station.prev();
@@ -32,9 +44,31 @@ public class RadioTest {
     }
 
     @ParameterizedTest
-    @CsvFileSource(files = "src/test/resources/shouldSetRadioStation.csv")
-    public void shouldSetRadioStation(int expected, int newCurrentRadioStation) {
+    @CsvFileSource(files = "src/test/resources/shouldPrevRadioStation.csv")
+    public void shouldPervRadioStation(int expected, int newCurrentRadioStation, int maxRadioStation) {
+        Radio station = new Radio(maxRadioStation);
+        station.currentRadioStation = newCurrentRadioStation;
+        station.prev();
+        int actual = station.currentRadioStation;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/shouldSetRadioStationDefault.csv")
+    public void shouldSetRadioStationDefault(int expected, int newCurrentRadioStation) {
         Radio station = new Radio();
+        station.setCurrentRadioStation(newCurrentRadioStation);
+        int actual = station.getCurrentRadioStation();
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(files = "src/test/resources/shouldSetRadioStation.csv")
+    public void shouldSetRadioStation(int expected, int newCurrentRadioStation, int maxRadioStation) {
+        Radio station = new Radio(maxRadioStation);
         station.setCurrentRadioStation(newCurrentRadioStation);
         int actual = station.getCurrentRadioStation();
 
